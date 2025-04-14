d4bf27f6efdd371ac98263c59229884af03b9ad5
DESKTOP-UJD7CL3\kiustudent <ramishvili.nino1@kiu.edu.ge>
2025-04-15 00:29:26 +0400

    ci

--- Test/app.test.js
+++ Test/app.test.js
@@ -5,22 +5,36 @@ const app = require('../index');
 const server = require('http').createServer(app);
 
 describe('GET /hello', () => {
+  let port;
+
   before(done => {
-    server.listen(3000, done);
+    server.listen(0, () => { 
+      const address = server.address();
+      port = address.port; 
+      done();
+    });
+
+    server.on('error', (err) => {
+      done(err); 
+    });
   });
 
   after(done => {
-    server.close(done);
+    if (server.listening) {
+      server.close(done);
+    } else {
+      done(); 
+    }
   });
 
   it('should return "Hello World!"', done => {
-    http.get('http://localhost:3000/hello', res => {
+    http.get(`http://localhost:${port}/hello`, res => {
       let data = '';
       res.on('data', chunk => { data += chunk; });
       res.on('end', () => {
         expect(data).to.equal('Hello World!');
         done();
       });
-    });
+    }).on('error', done);
   });
 });
