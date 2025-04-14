2a488cae25924d8e775dfc4d8ed59cf5e96cb9fa
DESKTOP-UJD7CL3\kiustudent <ramishvili.nino1@kiu.edu.ge>
2025-04-14 23:50:08 +0400

    Fix test configuration and update CI workflow

--- Test/app.test.js
+++ Test/app.test.js
@@ -1,10 +1,20 @@
 const chai = require('chai');
 const expect = chai.expect;
+const http = require('http');
+const app = require('../index');
+const server = require('http').createServer(app);
 
 describe('GET /hello', () => {
-  it('should return "Hello World!"', (done) => {
-    const http = require('http');
-    http.get('http://localhost:3000/hello', (res) => {
+  before(done => {
+    server.listen(3000, done);
+  });
+
+  after(done => {
+    server.close(done);
+  });
+
+  it('should return "Hello World!"', done => {
+    http.get('http://localhost:3000/hello', res => {
       let data = '';
       res.on('data', chunk => { data += chunk; });
       res.on('end', () => {
